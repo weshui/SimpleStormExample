a7cca21e9be7ee5473069a0bc4fb947995d6397d
silihui2 <silihui2@illinois.edu>
2015-12-06 01:32:16 -0600

    test

--- src/main/java/CountTopology/CumulateBolt.java
+++ src/main/java/CountTopology/CumulateBolt.java
@@ -45,7 +45,7 @@ public class CumulateBolt extends BaseBasicBolt {
         collector.emit(new Values(word, count));
         if (word.equals(key)) {
             System.out.println("Currnet count: " + counts.get(key));
-//            out.print(counts.get(key));
+            out.print(counts.get(key));
         }
 //        out.print(word);
     }
